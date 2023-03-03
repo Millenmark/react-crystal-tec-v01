cf7eda798919cdce48d86c0a392a38c432826c2d
Millen Mark Aquino <millenmark.iv@gmail.com>
2023-03-03 14:43:34 +0800

    I added swiper js

--- src/components/ClientCarousel/ClientCarousel.jsx
+++ src/components/ClientCarousel/ClientCarousel.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
 
 import './ClientCarousel.css'
 
@@ -7,6 +9,24 @@ const ClientCarousel = () => {
     <div className="client-carousel-section container">
       <div className="client-carousel-wrapper">
         <h2>Our Clients</h2>
+        <Swiper
+          spaceBetween={50}
+          slidesPerView={3}
+          onSlideChange={() => console.log('slide change')}
+          onSwiper={(swiper) => console.log(swiper)}
+          className="client-swiper"
+        >
+          <SwiperSlide><img className="client-swiper-image" src="../../../public/assets/images/pullman_logo.jpeg" alt="" /></SwiperSlide>
+
+          <SwiperSlide><img className="client-swiper-image" src="../../../public/assets/images/novotel_logo.jpeg" alt="" /></SwiperSlide>
+
+          <SwiperSlide><img className="client-swiper-image" src="../../../public/assets/images/novotel_logo.jpeg" alt="" /></SwiperSlide>
+          <SwiperSlide>Slide 3</SwiperSlide>
+          <SwiperSlide>Slide 4</SwiperSlide>
+          <SwiperSlide>Slide 2</SwiperSlide>
+          <SwiperSlide>Slide 3</SwiperSlide>
+          <SwiperSlide>Slide 4</SwiperSlide>
+        </Swiper>
       </div>
     </div>
   )
