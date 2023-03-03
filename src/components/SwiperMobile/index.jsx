5c56fbbb406d9abff0d3ee869f187f8601fe8955
Millen Mark Aquino <millenmark.iv@gmail.com>
2023-03-03 21:53:07 +0800

    I added loop

--- src/components/SwiperMobile/index.jsx
+++ src/components/SwiperMobile/index.jsx
@@ -1,5 +1,5 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Pagination } from 'swiper';
+import { Pagination , Autoplay  } from 'swiper';
 import 'swiper/css';
 import 'swiper/css/pagination';
 
@@ -7,13 +7,15 @@ const index = () => {
   return (
     <div className="swiper-mobile">
           <Swiper
-            modules={[Pagination]}
+            modules={[Pagination, Autoplay]}
             spaceBetween={20}
             slidesPerView={1}
             onSlideChange={() => console.log('slide change')}
             onSwiper={(swiper) => console.log(swiper)}
             className="client-swiper"
             pagination={{clickable: true}}
+            autoplay={{ delay: 2000, disableOnInteraction: false }}
+            loop={true}
           >
             <SwiperSlide><img className="client-swiper-image" src="../../../assets/images/pullman_logo.jpeg" alt="" /></SwiperSlide>
 
