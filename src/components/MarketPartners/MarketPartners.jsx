d54ddf7ec1c5dd31592f6e120391ce9bb903c707
Millen Mark Aquino <millenmark.iv@gmail.com>
2023-03-04 12:18:13 +0800

    I updated all over

--- src/components/MarketPartners/MarketPartners.jsx
+++ src/components/MarketPartners/MarketPartners.jsx
@@ -7,6 +7,10 @@ const MarketPartners = () => {
         <h2>Our Market Partners</h2>
         <div className="market-partners-logo">
           <div className="market-partners-logo-div">
+            <img className="partner-img" src="../../../public/assets/images/sap_logo.png" alt="" />
+          </div>
+
+          <div className="market-partners-logo-div">
             <img className="partner-img" src="../../../public/assets/images/microsoft_certified_logo.png" alt="" />
           </div>
 
@@ -15,7 +19,19 @@ const MarketPartners = () => {
           </div>
 
           <div className="market-partners-logo-div">
+            <img className="partner-img" src="../../../public/assets/images/vmware_logo.png" alt="" />
+          </div>
+
+          <div className="market-partners-logo-div">
             <img className="partner-img" src="../../../public/assets/images/sap_logo.png" alt="" />
+          </div>
+
+          <div className="market-partners-logo-div">
+            <img className="partner-img" src="../../../public/assets/images/oracle_logo.png" alt="" />
+          </div>
+
+          <div className="market-partners-logo-div">
+            <img className="partner-img" src="../../../public/assets/images/microsoft_certified_logo.png" alt="" />
           </div>
 
           <div className="market-partners-logo-div">
