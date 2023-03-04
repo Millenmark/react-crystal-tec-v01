4dfe7ae35dc46c301a2abe7bccdcfc0684374d10
Millen Mark Aquino <millenmark.iv@gmail.com>
2023-03-04 11:23:54 +0800

    added market partner logos

--- src/components/MarketPartners/MarketPartners.jsx
+++ src/components/MarketPartners/MarketPartners.jsx
@@ -6,7 +6,21 @@ const MarketPartners = () => {
       <div className="market-partners-wrapper">
         <h2>Our Market Partners</h2>
         <div className="market-partners-logo">
-          
+          <div className="market-partners-logo-div">
+            <img className="partner-img" src="../../../public/assets/images/microsoft_certified_logo.png" alt="" />
+          </div>
+
+          <div className="market-partners-logo-div">
+            <img className="partner-img" src="../../../public/assets/images/oracle_logo.png" alt="" />
+          </div>
+
+          <div className="market-partners-logo-div">
+            <img className="partner-img" src="../../../public/assets/images/sap_logo.png" alt="" />
+          </div>
+
+          <div className="market-partners-logo-div">
+            <img className="partner-img" src="../../../public/assets/images/vmware_logo.png" alt="" />
+          </div>
         </div>
       </div>
     </div>
