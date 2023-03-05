22041ee72536e8bbe6623ace34cab8e94f7cd78f
Millen Mark Aquino <millenmark.iv@gmail.com>
2023-03-05 18:25:01 +0800

    thank you page

--- src/pages/FormSubmittedPage.jsx
+++ src/pages/FormSubmittedPage.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import {BsCheckCircleFill} from 'react-icons/bs'
+import {FaTelegramPlane} from 'react-icons/fa'
 
+import './styles/FormSubmittedPage.css'
 const FormSubmittedPage = () => {
   return (
-    <div>FormSubmittedPage</div>
+    <div>
+      <div className="form-submitted-section">
+        <div className="form-submitted-wrapper container">
+          <div className="form-submitted-message">
+            <h1>Thank you! We Received Your Submission</h1>
+            <div className="form-submitted-icon">
+              <FaTelegramPlane className="form-submitted-plane-icon"/>
+              <BsCheckCircleFill className="form-submitted-check-icon"/>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
