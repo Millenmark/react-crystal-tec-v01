a8dea332959b400c66efedd54d9aa7c46e6c0a1c
Millen Mark Aquino <millenmark.iv@gmail.com>
2023-03-06 09:33:22 +0800

    Updated the App.jsx
    I tried using the React.lazy() to render a page when the user navigate

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
-import { BrowserRouter, Route, RouterProvider, Routes } from "react-router-dom"
+import React from 'react'
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 import Navbar from "./components/Navbar/Navbar"
 import Footer from "./components/Footer/Footer"
 
 import HomePage from "./pages/HomePage"
 import ProcessesPage from "./pages/ProcessesPage"
-import AboutPage from "./pages/AboutPage"
+// import AboutPage from "./pages/AboutPage"
 import ContactPage from "./pages/ContactPage"
 import ServicesPage from "./pages/ServicesPage"
 import ServiceCardPage from "./pages/ServiceCardPage"
 import Appointment from "./components/Appointment/Appointment"
+
+const LazyAboutPage = React.lazy(() => import('./pages/AboutPage'))
 
 
 function App() {
@@ -19,7 +22,11 @@ function App() {
         <div className="page-container">
           <Routes>
             <Route path="/" element={<HomePage/>}></Route>
-            <Route path="/about" element={<AboutPage/>}></Route>
+            <Route path="/about" element={
+              <React.Suspense fallback="Loading...">
+                <LazyAboutPage/>
+              </React.Suspense>
+            }></Route>
             <Route path="/services" element={<ServicesPage/>}></Route>
             <Route path="/processes" element={<ProcessesPage/>}></Route>
             <Route path="/contact" element={<ContactPage/>}></Route>
