725b6202fb93b41e75fde1b0c9e375b1c5486ddf
Millen Mark Aquino <millenmark.iv@gmail.com>
2023-03-06 09:12:28 +0800

    I updated the hero section
    I updated the vide background css file in the hero section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,19 +1,20 @@
 import React from 'react'
+import videoBg from '../../../public/assets/videos/hero_video.webm'
 
 import WithArrowButton from '../WithArrowButton/WithArrowButton'
 import './Hero.css'
 
 const Hero = () => {
   return (
-    <div className="video-background">
-    <video autoPlay muted loop>
-      <source src="/assets/videos/hero_video.webm" type="video/webm" />
-    </video>
+  <div className="video-background">
+    <video src={videoBg} autoPlay loop muted />
     <div className="hero-content">
+      <div className='content-item'>
       <h1>You focus business growth,<br/>we take care of your technology</h1>
       <br/>
       <h4>We are an IT Service Provider that delivers tailored and<br/> fully managed solutions that help you with any of your business IT needs. </h4>
       <WithArrowButton pathTo={"contact"} titleBtn={"Get Started"}/>
+      </div>
     </div>
   </div>
   )
